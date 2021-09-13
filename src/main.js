095312682f147c2fb5a965f1f33fb81220d33eac
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-09-13 09:03:57 -0300

    Limpando o projeto

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
+//Importando as rotas de navegação
+import {createRouter, createWebHistory} from "vue-router" 
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+
+    ]
+})
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
